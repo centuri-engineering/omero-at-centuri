fdbcf981ffc0459910c399259e92a6af044eb775
Guillaume Gay <guillaume@damcb.com>
2020-03-16 10:15:00 +0100

    better compose (with backup dir)

--- pyutils/roi_utils.py
+++ pyutils/roi_utils.py
@@ -18,7 +18,6 @@ def get_rois_as_labels(image, conn):
 
     """
 
-    image = conn.getObject("Image", 1)
     imshape = (image.getSizeX(), image.getSizeY())
 
     roi_service = conn.getRoiService()
@@ -33,13 +32,42 @@ def get_rois_as_labels(image, conn):
 
 
 def mask_from_polyon_shape(shape, imshape):
+    """Converts an omero roi `shape` (as returned by the `roi.getShape` method)
+    to a binay image with the pixels inside the shape set to 1.
+
+    Parameters
+    ----------
+    shape : `omero.model.PolygonI` instance
+    imshape : tuple, the shape of the output mask
+
+    Returns
+    -------
+    mask : `np.ndarray` of shape `imshape` and dtype uint8 with ones inside the
+       input shape.
+
+    """
+
     points = shape.getPoints()
 
-    points = np.array(([float(v) for v in l.split(",")] for l in points.val.split(" ")))
+    points = np.array(
+        [[float(v) for v in l.split(",") if v] for l in points.val.split(" ")]
+    )
     return polygon2mask(imshape, points).astype(np.uint8)
 
 
 def polygon_to_shape(polygon, z=0, t=0, c=0):
+    """Creates an omero roi shape from an ndarray polygon instance
+
+    Parameters
+    ----------
+
+    polygon : np.ndarray with shape (N, 2)
+        where N is the number of points in the polygon
+    z, c, t : ints
+        the Z, C and T position of the shape in the stack
+
+    """
+
     shape = omero.model.PolygonI()
     shape.theZ = rint(z)
     shape.theT = rint(t)
@@ -48,15 +76,34 @@ def polygon_to_shape(polygon, z=0, t=0, c=0):
     return shape
 
 
-def create_roi(img, polygons, conn):
+def register_shape_to_roi(image, polygon, conn, roi=None, z=0, t=0, c=0):
+    """Adds a polygon shape to an omero ROI. If no roi is provided,
+    creates it first.
+
+    Parameters
+    ----------
+    image : omero Image object
+    polygon : np.ndarray of shape (N, 2)
+    conn : connection to the omero db
+    roi : omero ROI, default None
+       if it is None, a new ROI will be created
+    z, t, c : position of the ROI in the stack (defaults to 0)
+
+
+    Returns
+    -------
+    roi: the roi
+
+    """
 
     updateService = conn.getUpdateService()
-    # create an ROI, link it to Image
-    roi = omero.model.RoiI()
-    # use the omero.model.ImageI that underlies the 'image' wrapper
-    roi.setImage(img._obj)
-    for poly in polygons:
-        shape = polygon_to_shape(poly, z=0, t=0, c=0)
-        roi.addShape(shape)
+    if roi is None:
+        # create an ROI, link it to Image
+        roi = omero.model.RoiI()
+        # use the omero.model.ImageI that underlies the 'image' wrapper
+        roi.setImage(image._obj)
+
+    shape = polygon_to_shape(polygon, z=z, t=t, c=c)
+    roi.addShape(shape)
     # Save the ROI (saves any linked shapes too)
     return updateService.saveAndReturnObject(roi)
