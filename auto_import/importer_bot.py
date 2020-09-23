bc25aaf28afbb60fb45cbffc7fdeb7f4caff71ee
Guillaume Gay <guillaume@damcb.com>
2020-09-23 18:00:23 +0200

    WIP auto annotation

--- auto_import/importer_bot.py
+++ auto_import/importer_bot.py
@@ -7,26 +7,46 @@
 import sys
 import argparse
 
+
 from importer_job import auto_import
+from annotation_job import auto_annotate
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("path", help="path to the directory you want to import into omero")
-parser.add_argument(
-    "-d",
-    "--dry_run",
-    help="do not perform the import, just output the preparation files",
-    action="store_true",
-)
+def main():
 
-parser.add_argument(
-    "-c",
-    "--use-cache",
-    help="Do not parse the directory again, perform import "
-    "from previously generated files",
-    action="store_true",
-)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "path", help="path to the directory you want to import into omero"
+    )
+    parser.add_argument(
+        "-d",
+        "--dry_run",
+        help="do not perform the import, just output the preparation files",
+        action="store_true",
+    )
 
-args = parser.parse_args()
-reset = not parser.use_cache
-auto_import(parser.path, parser.dry_run, reset)
+    parser.add_argument(
+        "-c",
+        "--use-cache",
+        help="Do not parse the directory again, perform import "
+        "from previously generated files",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "-a",
+        "--annotate",
+        help="automate annotations from yml files",
+        action="store_true",
+    )
+
+    args = parser.parse_args()
+    reset = not parser.use_cache
+    print("importing ... ")
+    conf = auto_import(parser.path, parser.dry_run, reset, clean=not parser.annotate)
+    if parser.annotate:
+        print("Annotating ... ")
+        auto_annotate(conf)
+
+
+main()
--- auto_import/importer_job.py
+++ auto_import/importer_job.py
@@ -3,23 +3,14 @@
 """Auto import script from a user directory
 """
 import os
-import json
 import logging
-import shutil
-import subprocess
+import tempfile
 from datetime import date
 
 from pathlib import Path
 
 import yaml
-import numpy as np
 import omero
-from omero.gateway import (
-    BlitzGateway,
-    TagAnnotationWrapper,
-    MapAnnotationWrapper,
-    CommentAnnotationWrapper,
-)
 
 from omero.util import import_candidates
 from omero.cli import CLI
@@ -28,6 +19,67 @@ log = logging.getLogger(__name__)
 logfile = logging.FileHandler("auto_importer.log")
 log.setLevel("INFO")
 log.addHandler(logfile)
+
+
+def auto_import(base_dir, dry_run=False, reset=True, clean=False):
+
+    base_dir = Path(base_dir)
+    conf = get_configuration(base_dir)
+
+    _, bulk_yml = tempfile.mkstemp(suffix=".yml", text=True)
+    log.info(f"creating bulk yaml {bulk_yml}")
+
+    _, tsv_file = tempfile.mkstemp(suffix=".tsv", text=True)
+    log.info(f"creating tsv_file {tsv_file}")
+
+    _, out_file = tempfile.mkstemp(suffix=".out", text=True)
+    log.info(f"creating out_file {out_file}")
+
+    conf["bulk_yml"] = bulk_yml
+    conf["tsv_file"] = tsv_file
+    conf["out_file"] = out_file
+
+    create_bulk_yml(bulk_yml=bulk_yml, dry_run=dry_run, path=tsv_file)
+
+    if not Path(tsv_file).is_file() or reset:
+        candidates = import_candidates.as_dictionary(base_dir.as_posix())
+        create_bulk_tsv(candidates, base_dir, conf)
+
+    perform_import(conf)
+    if clean:
+        for tmp in (bulk_yml, tsv_file, out_file):
+            os.remove(tmp)
+    return conf
+
+
+def perform_import(conf):
+
+    # TODO use `sudo` to login as root and not store the pswds
+    # see https://docs.openmicroscopy.org/omero/5.6.2/users/cli/sessions.html
+
+    cmd = [
+        "import",
+        "-s",
+        conf["server"],
+        "-p",
+        conf["port"],
+        "-u",
+        conf["username"],
+        "-w",
+        conf["password"],
+        "-g",
+        conf["group"],
+        "--exclude",
+        "--file",
+        Path(conf["out_file"]).absolute().as_posix(),
+        "--errs",
+        Path("err.txt").absolute().as_posix(),
+        "--bulk",
+        conf["bulk_yml"],
+    ]
+    cli = CLI()
+    cli.loadplugins()
+    cli.invoke(cmd)
 
 
 def get_configuration(pth):
@@ -47,7 +99,7 @@ def get_configuration(pth):
     For example:
 
     .. code::
-        {'base_path': 'User data root directory',
+        {'base_dir': 'User data root directory',
          'group': 'Prof Prakash',
          'password': 'XXXXX',
          'port': '4064',
@@ -77,13 +129,7 @@ def get_configuration(pth):
     raise FileNotFoundError("User configuration file not found")
 
 
-def collect_annotations(base_dir):
-
-    base_dir = Path(base_dir)
-    annotations = base_dir.glob("**/omero_annotation.yml")
-
-
-def create_bulk_yml(**kwargs):
+def create_bulk_yml(bulk_yml="bulk.yml", **kwargs):
     """Creates the bulk.yml file in the current directory
 
     Any keyword argument will update the default setting.
@@ -118,11 +164,11 @@ def create_bulk_yml(**kwargs):
     bulk_opts.update(kwargs)
 
     log.debug(f"bulk options: {bulk_opts}")
-    with open("bulk.yml", "w") as yml_file:
+    with open(bulk_yml, "w") as yml_file:
         yaml.dump(bulk_opts, yml_file)
 
 
-def create_bulk_tsv(candidates, base_path, conf):
+def create_bulk_tsv(candidates, base_dir, conf, out_file="files.tsv"):
 
     lines = []
     last_project = ""
@@ -130,7 +176,7 @@ def create_bulk_tsv(candidates, base_path, conf):
     # group candidates by directory
     paths = sorted(candidates, key=lambda p: Path(p).parent.as_posix())
     for fullpath in paths:
-        parts = Path(fullpath).relative_to(base_path).parts
+        parts = Path(fullpath).relative_to(base_dir).parts
         if len(parts) == 1:
             project = conf.get("project", "no_project")
             dataset = date.today().isoformat()
@@ -161,125 +207,5 @@ def create_bulk_tsv(candidates, base_path, conf):
 
         lines.append("\t".join((target, name, fullpath + "\n")))
 
-    with open("files.tsv", "w") as f:
+    with open(out_file, "w") as f:
         f.writelines(lines)
-
-
-def auto_import(directory, dry_run=False, reset=True):
-
-    directory = Path(directory)
-    conf = get_configuration(directory)
-    create_bulk_yml(dry_run=dry_run)
-
-    if not Path("files.tsv").is_file() or reset:
-        candidates = import_candidates.as_dictionary(directory.as_posix())
-        create_bulk_tsv(candidates, directory, conf)
-
-    all_ymls = directory.glob("**/*.yml")
-    annotation_ymls = [yml for yml in all_ymls if _is_annotation(yml)]
-    annotation_dirs = [yml.parent for yml in annotation_ymls]
-
-    for annotation_yml in annotation_ymls:
-        import_and_annotate(conf, annotation_yml)
-
-
-def _is_annotation(yml):
-    with open(yml, "r") as fh:
-        return "# omero annotation file" in fh.readline()
-
-
-def import_and_annotate(conf, annotation_yml):
-
-    cmd = [
-        "import",
-        "-s",
-        conf["server"],
-        "-p",
-        conf["port"],
-        "-u",
-        conf["username"],
-        "-w",
-        conf["password"],
-        "-g",
-        conf["group"],
-        "--file",
-        Path("out.txt").absolute().as_posix(),
-        "--errs",
-        Path("err.txt").absolute().as_posix(),
-        "--bulk",
-        "bulk.yml",
-    ]
-    cli = CLI()
-    cli.loadplugins()
-    cli.invoke(cmd)
-    auto_annotate(conf, "out.txt", annotation_yml)
-
-
-def auto_annotate(conf, out_file, annotation_yml):
-
-    conn = BlitzGateway(
-        conf["username"], conf["password"], host=conf["server"], port=conf["port"]
-    )
-    conn.connect()
-    datasets = get_datasets(conn, out_file)
-    for dataset_id in datasets:
-        annotate(conn, dataset_id, annotation_yml)
-
-
-def get_datasets(conn, out_file):
-
-    datasets = []
-
-    with open(out_file, "r") as out:
-        for line in out:
-            if not line.startswith("Image"):
-                continue
-            if "," in line:
-                line = line.split(",")[0]
-            im_id = int(line.split(":")[1])
-            datasets.append(conn.getObject("Image", im_id).getParent().getId())
-    return set(datasets)
-
-
-def annotate(conn, dataset_id, annotation_yml):
-    """Applies the annotations in `annotation_yml` to the dataset
-
-    Parameters
-    ----------
-    conn: An `omero.gateway.BlitzGateway` connection
-    dataset_id: int - the Id of the dataset to annotate
-    annnotation_yml: str or `Path` a yml file containing the annotation
-
-    """
-    dataset = conn.GetObject("Dataset", dataset_id)
-    log.info(f"\n")
-    log.info(f"Annotating dataset {dataset_id}")
-
-    with Path(annotation_yml).open("r") as ann_yml:
-        ann = yaml.safe_load(ann_yml)
-
-    key_value_pairs = list(ann.get("kv_pairs", {}).items())
-    if key_value_pairs:
-        log.info("Map annotations: ")
-        log.info("\n".join([f"{k}: {v}" for k, v in key_value_pairs]))
-        map_ann = MapAnnotationWrapper(conn)
-        namespace = omero.constants.metadata.NSCLIENTMAPANNOTATION
-        map_ann.setNs(namespace)
-        map_ann.setValue(key_value_pairs)
-        map_ann.save()
-        dataset.linkAnnotation(map_ann)
-
-    for tag in ann.get("tags", []):
-        log.info(f"Adding tag: {tag}")
-        tag_ann = TagAnnotationWrapper(conn)
-        tag_ann.setValue(tag)
-        tag_ann.save()
-        dataset.linkAnnotation(tag_ann)
-
-    comment = ann.get("comment", "")
-    if comment:
-        log.info(f"Adding comment: {comment}")
-        com_ann = CommentAnnotationWrapper(conn)
-        com_ann.setValue(comment)
-        com_ann.save()
-        dataset.linkAnnotation(com_ann)
