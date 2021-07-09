41967ee1a7db161c2f36e71c0f0b507b8bcf8d86
Guillaume Gay <guillaume@damcb.com>
2021-07-09 15:00:52 +0000

    better gitignore, sql env var

--- test_omero/data/run_impomero.sh
+++ test_omero/data/run_impomero.sh
@@ -7,6 +7,8 @@ cd server
 export OMERO_SERVER=localhost
 export OMERO_PORT=4064
 export OMERO_ROOT_PASSWORD=omero
+export IMPOMERO_DB=/data/impomero.sql
+
 
 source venv3/bin/activate
 python -m impomero -l /data/raw/dir0
